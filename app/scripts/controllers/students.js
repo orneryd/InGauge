0bea91126be1fd41f4456008ebde447f0eeb21ff
Michael Hwan <michael.a.hwan@gmail.com>
2014-04-26 11:20:09 -0700

    changed student page

--- app/scripts/controllers/students.js
+++ app/scripts/controllers/students.js
@@ -1,11 +1,22 @@
 'use strict';
 
 angular.module('sweduphxApp').controller('StudentsCtrl', ["$scope", "$http", "$socket", function ($scope, $http, $socket) {
-    $scope.student = {
-        name: null
-    };
+    $scope.currentStudent = null;
     
     $scope.currentPoll = null;
+
+    var getCurrentPoll = function(){
+      $http.get('/api/poll/active').success(function(poll) {
+        if (poll !== 'null') {
+          $scope.poll = poll;
+        }
+      });
+    };
+
+    $scope.selectStudent = function(student){
+      $scope.currentStudent = student;
+      getCurrentPoll()
+    };
     
     $scope.currentPollResult = {
         student: {
